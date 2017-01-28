facc39ce87521b9e6f4357dc226b9afbd134d0a3
Jace Benson <jace.benson@gmail.com>
2017-01-28 10:48:26 -0600

    inserted trys

--- app/routes/api.js
+++ app/routes/api.js
@@ -4,60 +4,73 @@ var MongoClient = mongodb.MongoClient;
 var mongoURI = process.env.MONGOLAB_URI || require('./.env').uri;
 function findByURL(requestedUrl, response) {
     //verify url is valid;
-    var validUrl = require('valid-url');
-    if (validUrl.isWebUri(requestedUrl)) {
-        var resultFound = { id: false };
+    try {
+        var validUrl = require('valid-url');
+        if (validUrl.isWebUri(requestedUrl)) {
+            var resultFound = { id: false };
+            MongoClient.connect(mongoURI, function (err, db) {
+                if (err) throw err;
+                db.collection('shorts').findOne({ original_url: requestedUrl }, function (err, item) {
+                    console.log(typeof item);
+                    console.log(JSON.stringify(item));
+                    if (item) {
+                        response.send(JSON.stringify(item, '', '   '));
+                    } else {
+                        insert(requestedUrl, response);
+                    }
+                });
+                db.close();
+            });
+        } else {
+            response.send(JSON.stringify({ error: "Not valid URL." }, '', '   '));
+        }
+    } catch (err) {
+        response.send(JSON.stringify(err, '', '   '));
+    }
+}
+function insert(requestedUrl, response) {
+    try {
         MongoClient.connect(mongoURI, function (err, db) {
             if (err) throw err;
-            db.collection('shorts').findOne({ original_url: requestedUrl }, function (err, item) {
-                console.log(typeof item);
-                console.log(JSON.stringify(item));
+            console.log('Inserting url: ' + requestedUrl);
+            var id = db.collection('shorts').count({}, function (error, numOfDocs) {
+                if (err) throw err;
+                if (numOfDocs === null) {
+                    numOfDocs = 0;
+                }
+                item = { original_url: requestedUrl, id: numOfDocs };
+                db.collection('shorts').insert(item);
+                response.end(JSON.stringify(item, '', '   '));
+                db.close();
+            });
+        });
+    } catch (err) {
+        response.send(JSON.stringify(err, '', '   '));
+    }
+}
+function findByID(input, response) {
+    try {
+        MongoClient.connect(mongoURI, function (err, db) {
+            if (err) throw err;
+            console.log('looking for ' + input);
+            db.collection('shorts').findOne({ id: parseInt(input, 10) }, function (err, item) {
+                //db.collection('shorts').findOne({ id: '"' + input + '"' }, function (err, item) {
                 if (item) {
-                    response.send(JSON.stringify(item, '', '   '));
+                    //response.end(JSON.stringify(item, '', '   '));
+                    response.writeHead(302, {
+                        'Location': item.original_url
+                    });
+                    response.end();
                 } else {
-                    insert(requestedUrl, response);
+                    response.end(JSON.stringify({ error: 'Could not find record.' }, '', '   '));
                 }
             });
             db.close();
         });
-    } else {
-        response.send(JSON.stringify({ error: "Not valid URL." }, '', '   '));
+
+    } catch (err) {
+        response.send(JSON.stringify(err, '', '   '));
     }
-}
-function insert(requestedUrl, response) {
-  MongoClient.connect(mongoURI, function (err, db) {
-    if (err) throw err;
-    console.log('Inserting url: ' + requestedUrl);
-    var id = db.collection('shorts').count({}, function (error, numOfDocs) {
-      if (err) throw err;
-      if (numOfDocs === null) {
-        numOfDocs = 0;
-      }
-      item = { original_url: requestedUrl, id: numOfDocs };
-      db.collection('shorts').insert(item);
-      response.end(JSON.stringify(item, '', '   '));
-      db.close();
-    });
-  });
-}
-function findByID(input, response) {
-  MongoClient.connect(mongoURI, function (err, db) {
-    if (err) throw err;
-    console.log('looking for ' + input);
-    db.collection('shorts').findOne({ id: parseInt(input, 10) }, function (err, item) {
-      //db.collection('shorts').findOne({ id: '"' + input + '"' }, function (err, item) {
-      if (item) {
-        //response.end(JSON.stringify(item, '', '   '));
-        response.writeHead(302, {
-          'Location': item.original_url
-        });
-        response.end();
-      } else {
-        response.end(JSON.stringify({ error: 'Could not find record.' }, '', '   '));
-      }
-    });
-    db.close();
-  });
 }
 module.exports = function (app) {
     var client_id = process.env.CLIENT_ID || require('./.env').client_id;
@@ -118,11 +131,11 @@ module.exports = function (app) {
             var term = encodeURIComponent(req.query.term);
             //start mongodb insert call
             var requestedUrl = req.path.replace(/^\/new\//, '');
-            findByURL(requestedUrl,res);
+            findByURL(requestedUrl, res);
         });
     app.route('/:num')
         .get(function (req, res) {
             console.log(req.path);
-            findByID(req.path.replace(/\//,''),res);
+            findByID(req.path.replace(/\//, ''), res);
         });
 };
